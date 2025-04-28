9510e3f5f891fdd83ffab9a3bcc1ad34d1e44c48
psuntharanund <pawat.suntharanund@gmail.com>
2025-04-28 17:48:40 -0400

    Changed the RPS game to be a best of five format.

--- rock-paper-scissors-game/gamescript.js
+++ rock-paper-scissors-game/gamescript.js
@@ -1,5 +1,5 @@
 function startGame(){
-    console.log("Game has started!");
+    startButton.disabled = true;
     console.log(cpuChoiceNum);
     if (cpuChoiceNum === 1) {
         cpuChoiceStr = "Rock";
@@ -21,32 +21,60 @@ function checkGuess(){
         return;
     }
     if (userGuess.toLowerCase() === cpuChoiceStr.toLowerCase()){
-        inputs.textContent = "Congratulations, you've guessed correctly!"
+        playerScore++;
+        gameBestOfFive++;
+        if (playerScore < 3) {
+            inputs.textContent = "Congratulations, you've guessed correctly! The score is now (Player: " + playerScore + " CPU: " + cpuScore + ")"
+        } else {
+            inputs.textContent = "Final score: (Player: " + playerScore + " CPU: " + cpuScore + ")"
+        }
         gameOver();
     } else {
-        inputs.textContent = "Sorry, you have lost the game."
+        cpuScore++;
+        gameBestOfFive++;
+        if (cpuScore < 3) {
+            inputs.textContent = "Sorry, you have lost! The score is now (Player: " + playerScore + " CPU: " + cpuScore + ")"
+        } else {
+            inputs.textContent = "Final score: (Player: " + playerScore + " CPU: " + cpuScore + ")"
+        }
         gameOver();
     }
 }
 
 function gameOver(){
-    startButton.disabled = true;
     guessField.disabled = true;
     submitButton.disabled = true;
-    resetButton = document.createElement("button");
-    resetButton.textContent = "Reset Game";
-    document.body.appendChild(resetButton);
-    resetButton.addEventListener("click", resetGame);
+    if (playerScore === 3 || cpuScore === 3 || gameBestOfFive > 5){
+        inputs.textContent = "Final score: (Player: " + playerScore + " CPU: " + cpuScore + ")"
+        resetButton = document.createElement("button");
+        resetButton.textContent = "Reset Game";
+        document.body.appendChild(resetButton);
+        resetButton.addEventListener("click", resetGame);
+    } else {
+        nextButton = document.createElement("button");
+        nextButton.textContent = "Next round";
+        document.body.appendChild(nextButton);
+        nextButton.addEventListener("click", resetGame);
+    }
 }
 
 function resetGame(){
     startButton.disabled = false;
     guessField.disabled = false;
     submitButton.disabled = false;
-    resetButton.parentNode.removeChild(resetButton);
-    inputs.textContent = "";
-    guessField.value = "";
-    cpuChoiceNum = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+    if (gameBestOfFive > 5 || playerScore === 3 || cpuScore === 3){
+        resetButton.parentNode.removeChild(resetButton);
+        inputs.textContent = "";
+        guessField.value = "";
+        playerScore = 0;
+        cpuScore = 0;
+        gameBestOfFive = 1;
+    } else {
+        nextButton.parentNode.removeChild(nextButton);
+        guessField.value = "";
+    }
+    cpuChoiceNum = Math.floor(Math.random() * 3) + 1;
+    startGame();
 }
 
 const startButton = document.getElementById("startButton");
@@ -54,7 +82,10 @@ const submitButton = document.getElementById("submitButton");
 const guessField = document.querySelector(".guessField");
 const guessSubmit = document.querySelector(".guessSubmit");
 const inputs = document.querySelector(".inputs");
-let cpuChoiceNum = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+let playerScore = 0;
+let cpuScore = 0;
+let gameBestOfFive = 1;
+let cpuChoiceNum = Math.floor(Math.random() * 3) + 1;
 let cpuChoiceStr;
 
 startButton.addEventListener("click", startGame);
